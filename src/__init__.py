057f140377c2b3d81e17db141b9f95baa94c152f
Kuldeep Sahu <kuldeepsahu0304@gmail.com>
2024-11-19 15:07:11 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+Customer Churn Prediction Package
+This package contains modules for data preprocessing, feature engineering,
+model training, and evaluation.
+"""
+
+__all__ = ["data_preprocessing", "feature_engineering", "model_training", "evaluation"]
